e09b012cdf84f048569c7412d6ec915696052118
lucky4wu <crazywu@126.com>
2022-08-13 10:35:08 +0800

    init Jenkinsfile

--- src/main/java/com/example/demo5/Demo5Application.java
+++ src/main/java/com/example/demo5/Demo5Application.java
@@ -3,12 +3,15 @@ package com.example.demo5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class Demo5Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Demo5Application.class, args);
         System.out.println("hello world");
+        System.in.read();
     }
 
 }
